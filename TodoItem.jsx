f7ffd21cab3ee17e439e5a4c5ff7019f63593098
TahreemShahid <tahreemshahid2212@gmail.com>
2025-06-24 01:22:28 +0500

    Add files via upload

---
+++ TodoItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TodoItem.css";
+
+function TodoItem({ todo, onToggle, onDelete }) {
+  return (
+    <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => onToggle(todo.id)}
+      />
+      <span>{todo.text}</span>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </li>
+  );
+}
+
+export default TodoItem; 
